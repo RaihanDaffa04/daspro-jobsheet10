d4236df5e0902b8825b7aa6e323211ec52d6a8d9
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-11-07 12:16:32 +0800

    commit modifikasi pertanyaan

--- SIAKAD24.java
+++ SIAKAD24.java
@@ -3,27 +3,34 @@ import java.util.Scanner;
 public class SIAKAD24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[][] nilai = new int[4][3];
-        for (int i = 0; i < nilai.length; i++) {
+        int mahasiswa = 0, mataKuliah = 0;
+        System.out.print(" Masukkan jumlah Mahasiswa: ");
+        mahasiswa = input.nextInt();
+        System.out.print(" Masukkan Jumlah Mata Kuliah : ");
+        mataKuliah = input.nextInt();
+        int[][] nilai = new int[mahasiswa][mataKuliah];
+        
+        for (int i = 0; i < mahasiswa; i++) {
             System.out.println("Input nilai mahasiswa ke-" + (i + 1));
-            double totalPerSiswa = 0;
-            for (int j = 0; j < nilai[i].length; j++) {
+            double totalPerMahasiswa = 0;
+            for (int j = 0; j < mataKuliah; j++) {
                 System.out.println("Nilai mata kuliah " + (j + 1) + ": ");
                 nilai[i][j] = input.nextInt();
-                totalPerSiswa += nilai[i][j];
+                totalPerMahasiswa += nilai[i][j];
             }
-            System.out.println("Nilai rata-rata: " + totalPerSiswa / 3);
+            System.out.println("Nilai rata-rata: " + totalPerMahasiswa / mahasiswa);
         }
+        
         System.out.println("\n=====================================");
         System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < mataKuliah; j++) {
             double totalPerMatkul = 0;
 
-            for (int i = 0; i < 4; i++) {
+            for (int i = 0; i < mahasiswa; i++) {
                 totalPerMatkul += nilai[i][j];
             }
-            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul / 4);
+            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPerMatkul / mataKuliah);
         }
     }
 }
